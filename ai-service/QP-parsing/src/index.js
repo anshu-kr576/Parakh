c1a2f09f4bb2a56a2d96c19032ecd19ab6367dc8
AadityaPokhriyal <pokhriyal.adi@gmail.com>
2026-07-04 23:08:28 +0530

    QP-parsing folder update (#6)
    * Fixed .gitignore, added .gitattributes, added jsonwebtoken, node modules and .env are removed from repo
    
    * minor cleanup of files

--- ai-service/QP-parsing/src/index.js
+++ ai-service/QP-parsing/src/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 dotenv.config();
 
 const app=express();
 const port=process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Testing the setup...");
-});
+
+//algo to be used in JWT authentication RS256
+
+
 
 app.listen(port,()=>{
     console.log(`Listening to port ${port}...`);
